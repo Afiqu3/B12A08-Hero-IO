5ba0ef9be78e9d0a4f6836b3e7e03e1dba732976
Afiqu3 <gahossainj@gmail.com>
2025-10-08 00:18:45 +0600

    fix navbar responsive problem

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import { IoIosMenu } from "react-icons/io";
 import { TbXboxXFilled } from "react-icons/tb";
 import { Link } from "react-router";
 import "./links.css";
-import logoImg from "../../assets/logo.png"
-import gitImg from "../../assets/github.png"
+import logoImg from "../../assets/logo.png";
+import gitImg from "../../assets/github.png";
 
 const navigationData = [
   {
@@ -50,24 +50,44 @@ const Navbar = () => {
             ></IoIosMenu>
           )}
           <h3 className="text-xl font-bold">
-            <Link to={`/`} className="flex items-center gap-x-1 bg-gradient-to-r from-[#632ee3] to-[#9f62f2] bg-clip-text text-transparent">
-            <img className="h-10 w-10" src={logoImg} alt="" />
-            HERO.IO</Link>
+            <Link
+              to={`/`}
+              className="flex items-center gap-x-1 bg-gradient-to-r from-[#632ee3] to-[#9f62f2] bg-clip-text text-transparent"
+            >
+              <img className="h-10 w-10" src={logoImg} alt="" />
+              HERO.IO
+            </Link>
           </h3>
         </div>
 
         <ul className="md:flex hidden gap-x-8 font-medium">{links}</ul>
 
-        <div>
-          <Link to={'https://github.com/Afiqu3'} target="_blank"
-        rel="noopener noreferrer" className="btn bg-gradient-to-r from-[#632ee3] to-[#9f62f2] text-white rounded px-4 py-3">
-            <img src={gitImg} alt="" />Contribute
+        <div className="md:block hidden">
+          <Link
+            to={"https://github.com/Afiqu3"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn bg-gradient-to-r from-[#632ee3] to-[#9f62f2] text-white rounded px-4 py-3"
+          >
+            <img src={gitImg} alt="" />
+            Contribute
           </Link>
         </div>
       </nav>
       {isOpen && (
         <div className="md:hidden mt-2 mb-5 ml-10 space-y-3 max-w-45 relative h-20">
           <ul className={`space-y-3 animation`}>{links}</ul>
+          <div>
+            <Link
+              to={"https://github.com/Afiqu3"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-sm bg-gradient-to-r from-[#632ee3] to-[#9f62f2] text-white rounded px-4 py-3 animation"
+            >
+              <img src={gitImg} alt="" />
+              Contribute
+            </Link>
+          </div>
         </div>
       )}
     </div>
